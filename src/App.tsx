59deffe8ec507f2724978b90d49754bc15236dd2
Keith <keithharryman75@gmail.com>
2022-07-23 16:26:28 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user: any) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Keith',
+  lastName: 'Harryman'
+};
+
+const userGreeting = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to the Learn Facts Quick Programming Blog!
         </p>
         <a
           className="App-link"
